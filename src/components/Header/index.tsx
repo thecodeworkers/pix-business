e9a87aa6d1338670cb715d8523a49fe2e2b38861
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-07-31 15:08:45 -0400

    some styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="_image">
           <img src="https://loremflickr.com/50/40" alt="_icon" />
         </div>
-        <div>Holy chicken</div>
+        <div className="_text">Holy chicken</div>
       </div>
       <div className="_actions _subcontainer">
         <div className="_icon"><Notification color='#c9c9c9' width='22' height='27' /></div>
