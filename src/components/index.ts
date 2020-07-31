598031647251a772cb787916184f5341c67a2a78
gibercode <giberlara@gmail.com>
2020-07-31 19:46:29 -0400

    qr code

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Header } from './Header';
 export { default as ActionButton } from './ActionButton'
 export { default as ExportButton } from './ExportButton';
 export { default as IconTabs } from './IconTabs';
+export { default as CodeQR } from './QRcode';
