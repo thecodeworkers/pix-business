d2157e83d8066bf147c6f34a7e378f7b307ecd96
Victor Caceres <caceres.vc95@gmail.com>
2020-07-31 10:21:54 -0400

    export example

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { Main } from './views';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { saveUser } from './store/actions';
-
-saveUser();
 
 export default () => (
   <Provider store={store}>
