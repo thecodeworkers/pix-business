b25790a4ea6494be74f0f02a31637685dc47837d
Victor Caceres <caceres.vc95@gmail.com>
2020-07-31 15:18:10 -0400

    change side bar

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Dashboard from '../Dashboard';
 import { Router } from '@reach/router';
+import { SideBar } from '../../components';
 
 export default () => (
+  <>
+  <SideBar/>
   <Router>
     <Dashboard path="/dashboard"/>
   </Router>   
+  </>
 );
