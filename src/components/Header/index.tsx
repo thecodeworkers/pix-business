2d3c7b253c59e10dd6109f4c94667b4282c99e63
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-07-31 12:32:03 -0400

    header structure and dashboard lateral margin

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import './styles.scss';
 
 const Header = () => {
   return (
-    <div>Hello World</div>
+    <div className="_container">
+      <div className="_enterprise _subcontainer">
+        <div>
+          <img src="https://loremflickr.com/50/50" alt="_icon" />
+        </div>
+        <div>Holy chicken</div>
+      </div>
+      <div className="_actions _subcontainer">
+        <div>NOTF</div>
+        <div>USER</div>
+      </div>
+    </div>
   )
 }
 
--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import { RouteComponentProps } from "@reach/router";
 import { Header } from "../../components";
+import './styles.scss'
 
 const Dashboard: FC<RouteComponentProps> = () => (
   <div>
-    <Header />
-    <div>Dashboard</div>
+    <section className="_dcontainer">
+      <Header />
+      <div>Dashboard</div>
+    </section>
   </div>
 );
 
