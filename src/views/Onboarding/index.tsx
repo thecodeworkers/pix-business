3964b51f196cf687ec9dafe4ef16ca82bea7e33f
Victor Caceres <caceres.vc95@gmail.com>
2020-08-01 20:34:53 -0400

    validate register

--- src/views/Onboarding/index.tsx
+++ src/views/Onboarding/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Router } from '@reach/router';
+import React, { useEffect } from 'react';
+import { Router, navigate } from '@reach/router';
 import Login from '../Login';
 import Register from '../Register';
+import { connect } from 'react-redux';
+import { Props } from './interface';
 
-export default () => (
-  <Router>
-    <Login path="/"/>
-    <Register path="/register"/>
-  </Router>
-);
+const Onboarding = ({ commerce }: Props) => {
+  useEffect(() => {
+    if(!commerce.commerce) navigate('/register');
+  }, []);
+
+  return (
+    <Router>
+      <Login path="/"/>
+      <Register path="/register"/>
+    </Router>
+  );
+}
+
+const mapStateToProps = ({ commerce }: Props) => ({ commerce });
+
+export default connect(mapStateToProps)(Onboarding);
