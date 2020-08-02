4c8e7bf5fc328adc3c05b7985c1098cef000db0d
Victor Caceres <caceres.vc95@gmail.com>
2020-08-02 11:33:37 -0400

    change

--- src/store/product/reducer.ts
+++ src/store/product/reducer.ts
@@ -2,7 +2,62 @@ import { DispatchProps } from '../../interfaces';
 import { CREATE_PRODUCT } from './action-types';
 
 const initialState: any = {
-  products : []
+  products : [
+    {
+      name: 'Holly Chicken',
+      sku: 'EF52000',
+      tag: 'Inventory',
+      description: 'chicken burger',
+      selfPrice: '10 USDC',
+      cost: 66.245,
+      quantity: 5
+    },
+    {
+      name: 'Holly Chicken',
+      sku: 'EF52000',
+      tag: 'Inventory',
+      description: 'chicken burger',
+      selfPrice: '10 USDC',
+      cost: 66.245,
+      quantity: 5
+    },
+    {
+      name: 'Holly Chicken',
+      sku: 'EF52000',
+      tag: 'Inventory',
+      description: 'chicken burger',
+      selfPrice: '10 USDC',
+      cost: 66.245,
+      quantity: 5
+    },
+    {
+      name: 'Holly Chicken',
+      sku: 'EF52000',
+      tag: 'Inventory',
+      description: 'chicken burger',
+      selfPrice: '10 USDC',
+      cost: 66.245,
+      quantity: 5
+    },
+    {
+      name: 'Holly Chicken',
+      sku: 'EF52000',
+      tag: 'Inventory',
+      description: 'chicken burger',
+      selfPrice: '10 USDC',
+      cost: 66.245,
+      quantity: 5
+    },
+    {
+      name: 'Holly Chicken',
+      sku: 'EF52000',
+      tag: 'Inventory',
+      description: 'chicken burger',
+      selfPrice: '10 USDC',
+      cost: 66.245,
+      quantity: 5
+    },
+  ]
 }
 
 export default (state = initialState, { type, payload }: DispatchProps) => {
@@ -10,8 +65,8 @@ export default (state = initialState, { type, payload }: DispatchProps) => {
     case CREATE_PRODUCT:
     return {
       products: [
-        ...state.producs,
-        ...payload.produc
+        ...state.products,
+        ...payload.product
       ]
     }
 
