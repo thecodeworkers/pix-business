05e9af87318f4bc02d527135dfcab1497dc95312
Victor Caceres <caceres.vc95@gmail.com>
2020-08-02 01:12:31 -0400

    changes

--- src/store/activity/action.ts
+++ src/store/activity/action.ts
@@ -1,4 +1,7 @@
-import { actionObject } from "../../utils";
-import { GET_ACTIVITIES } from "./action-types";
+import { actionObject } from '../../utils';
+import { GET_ACTIVITIES, GET_BACKUP_ACTIVITIES, SAVE_ACTIVITY, FILTER_ACTIVITIES } from './action-types';
 
 export const getActivities = () => actionObject(GET_ACTIVITIES);
+export const saveActivity = (activity: any) => actionObject(SAVE_ACTIVITY, { activity: [activity] });
+export const getBackupActivities = () => actionObject(GET_BACKUP_ACTIVITIES);
+export const filterActivities = (filter: string) => actionObject(FILTER_ACTIVITIES, filter);
