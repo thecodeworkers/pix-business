210925b36e039d33283ca3744c9456116f5a73a1
MauriceMiot <maurice.yggdrasil@gmail.com>
2020-08-02 12:08:01 -0400

    profile finished

--- src/store/bankAccount/action-types.ts
+++ src/store/bankAccount/action-types.ts
@@ -1,1 +1,2 @@
 export const GET_BANK_ACCOUNT: string = 'GET_BANK_ACCOUNT';
+export const SAVE_BANK_ACCOUNT: string = 'SAVE_BANK_ACCOUNT';
--- src/store/bankAccount/reducer.ts
+++ src/store/bankAccount/reducer.ts
@@ -1,8 +1,8 @@
 import { DispatchProps } from '../../interfaces';
-import { GET_BANK_ACCOUNT } from './action-types';
+import { GET_BANK_ACCOUNT, SAVE_BANK_ACCOUNT } from './action-types';
 
 const initialState: any = {
-  results: [
+  bankAccounts: [
     {
       branchAddress: '80 SW 8th St, Suite 100, Miami, FL, 33130',
       checkingAccount: ' 609501090',
@@ -36,10 +36,19 @@ const initialState: any = {
   ]
 }
 
-export default (state = initialState, { type }: DispatchProps) => {
+export default (state = initialState, { type, payload }: DispatchProps) => {
   switch(type) {
     case GET_BANK_ACCOUNT:
     return state;
+
+    case SAVE_BANK_ACCOUNT:
+      console.log(payload);
+    return {
+      bankAccounts: [
+        ...state.bankAccounts,
+        ...payload.bankAccount
+      ],
+    };
 
     default:
     return state;
