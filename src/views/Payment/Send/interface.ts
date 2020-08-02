5745c89224b95a7a60cf7b581e93dc51b2b32bc5
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-08-02 12:53:05 -0400

    add interface

--- src/views/Payment/Send/interface.ts
+++ src/views/Payment/Send/interface.ts
@@ -3,11 +3,13 @@ import { RouteComponentProps } from "@reach/router";
 
 export interface StateProps {
   wallet: WalletProps
-  bankAccount: any
+  bankAccount: any,
+  counterparty: any
 }
 
 export interface Props extends RouteComponentProps {
   action: any
   wallet: WalletProps
-  bankAccount: any
+  bankAccount: any,
+  counterparty: any
 }
